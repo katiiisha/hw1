70efda7436b37f04866c3fb78d2f008acd6f4fb2
katiiisha <mendeleevakate2002@gmail.com>
2022-11-28 22:12:19 +0300

    Выполнила дз
    Доделала скрипт с выводами единиц, десятков и сотен

---
+++ js.js
@@ -0,0 +1,12 @@
+let number = prompt("Введите 3-x значное число число");
+let units = number%10;
+let a = (number/10)-(units*0.1);
+let dozens = a%10;
+let b = number%100;
+let hundreds = (number/100)-(b*0.01);
+alert (units)
+alert (dozens)
+alert(hundreds)
+alert(":)")
+
+//153 > 153%10=3 > 153/10=15,3-0.3=15 > 153/100=1.53
